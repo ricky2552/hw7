45ab13ad1399f295bb47334dd02d63ae8b04c7bf
Ricky L <Ricky L>
2017-02-28 11:34:45 -0800

    work

---
+++ hw7.js
@@ -0,0 +1,29 @@
+var locations = [
+    [
+        "David Strong Building",
+        48.464801,
+         -123.313539
+    ],
+    [
+    		"ECS",
+        48.461188,
+        -123.311802
+    ]
+]
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: new google.maps.LatLng(48.463649,  -123.311951),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+    }
